3a6820eb51792644058ed87dd99c9f7b566b2a04
Rayana Vital <rayanavital@gmail.com>
2022-12-11 12:10:40 -0300

    Add functions to show, init and end board game

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,3 +1,4 @@
+import { JogoDaVelhaService } from './shared/jogo-da-velha.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor( private jogoDaVelhaService: JogoDaVelhaService) { }
 
   ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
   }
+
+  /**
+   * Retorna se a tela de inicio deve ser exibida.
+   *
+   * @return boolean
+   */
+
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+  /**
+   * Retorna se o tabuleiro deve ser exibido.
+   *
+   * @return boolean
+   */
+
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  /**
+   * Retorna se a tela de fim de jogo deve ser exibida.
+   *
+   * @return boolean
+   */
+
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+
+
 
 }
